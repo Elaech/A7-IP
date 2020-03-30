b49396e5ded5f478e2963ebcd9aa4a17e9e4a459
Vlad Dexamir <vlad.dexamir@3pillarglobal.com>
2020-03-30 18:44:19 +0300

    FrontEnd Project Setup

---
+++ Final Project/Client/fiimentor/next.config.js
@@ -0,0 +1,9 @@
+const withCSS = require('@zeit/next-css')
+
+module.exports = withCSS({
+	cssModules: true,
+	cssLoaderOptions: {
+		importLoaders: 1,
+		localIdentName: '[local]___[hash:base64:5]',
+	},
+})
---
+++ Final Project/Client/fiimentor/pages/index.jsx
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import React from 'react'
+
+const Home = () => (
+  <>
+  <Head>
+  <title>FIIMentor</title>
+		</Head>
+  <h1>FIIMentor</h1>
+	</>
+);
+export default Home
