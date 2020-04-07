44374136a1faa3b0480c82fb0f680047330747c1
AdelinaAlicia <62259221+AdelinaAlicia@users.noreply.github.com>
2020-04-07 23:03:07 +0300

    new

--- Final Project/Client/fiimentor/main/components/Loginform.jsx
+++ Final Project/Client/fiimentor/main/components/Loginform.jsx
@@ -1,77 +1,115 @@
-import React from 'react'
-import { Formik } from 'formik'
-import FormControl from '@material-ui/core/FormControl';
-import Checkbox from '@material-ui/core/Checkbox';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Button from '@material-ui/core/Button';
-import TextField from '@material-ui/core/TextField';
-import MenuItem from '@material-ui/core/MenuItem';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import Search from '@material-ui/icons/Search';
-import Select from '@material-ui/core/Select';
-import styled from 'styled-components'
-
-const DivForm = styled.div`
-  width: 400px;
-  text-align:center;
-  padding: 0 20px;
-  padding-bottom: 20px;
-  border: grey solid 1px; 
-  border-radius: 20px;
-  background:#F6F6F6;
-`;
+import React from 'react';
+import { Field, Formik, FormikProps } from 'formik';
+import * as Yup from 'yup';
+import {
+  FormGroup,
+  Button,
+} from '@material-ui/core';
+import { MessageFormContainer } from './MessageFormStyles';
+import {user} from '../../core/domain/user'
+import {Checkbox} from '../Generics/CheckBox';
+import {EmailInput} from '../Generics/EmailInput';
+import {passwordImput} from '../Generics/EmailInput';
 
 
+interface LoginFormValues {
+    email: string;
+    password: string;
+  }
 
-class Loginform extends React.Component {
-    //handleSubmit()
+
+const initialValues: LoginFormValues = {
+    email: '',
+    password: ''
+  };
+
+const validationSchema: Yup.Schema<LoginFormValues> = Yup.object().shape({
+    email: Yup.string()
+      .email()
+      .required('Field is required'),
+    password: Yup.string()
+      .min(user.passwordConstraint.min, 'Parola trebuie sa aiba cel putin 6 caractere')
+      .max(user.passwordConstraint.min, 'Parola trebuie sa aiba cel mult 18 caractere')
+      .required('Field is required'),
+  });
+
+const SignupForm = () => {
+    const formik = useFormik({
+      initialValues: { email: "" },
+      onSubmit: values => {
+        alert(JSON.stringify(values, null, 2));
+      }
+    });
+    return (
+            <Form onSubmit={handleSubmit}>
+              <Field
+                name="email"
+                label="Email*"
+                placeholder="Email"
+                component={EmailInput}
+              />
+              <Field
+                name="password"
+                label="Password*"
+                placeholder="Password"
+                component={PasswordInput}
+              />
+              <LoginButtonContainer>
+                <Button type="submit" style={loginButtonColor}>Login</Button>
+              </LoginButtonContainer>
+              <LoginOptionsContainer>
+                <Button size="sm" style={optionsButton} onClick={this.openReset}>Forgot Password</Button>
+                <Button size="sm" style={registerButton} onClick={this.openRegister}>Register</Button>
+              </LoginOptionsContainer>
+            </Form>
+    );
+  };
+  
+  function App() {
+    return <SignupForm />;
+  }
+  
+  const rootElement = document.getElementById("root");
+  ReactDOM.render(<App />, rootElement);
+
+  class Loginform extends React.Component {
+ 
+    handleSubmit (values: MessageFormValues) {
+      console.log(values);
+}
     render() {
         return (
-            <DivForm>
+          <MessageFormContainer>
                 <Formik
-                    initialValues={{ contact: '', message: '' }}
-                    validate={values => {
-                        const errors = {};
-                        if (!values.contact) {
-                            errors.contact = 'Required';
-                        } else if (
-                            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-                        ) {
-                            errors.email = 'Invalid contact';
-                        }
-                        return errors;
-                    }}
-                    onSubmit={(values, { setSubmitting }) => {
-                        setTimeout(() => {
-                            alert(JSON.stringify(values, null, 2));
-                            setSubmitting(false);
-                        }, 400);
-                    }}
+                  initialValues={initialValues}
+                  validationSchema={validationSchema}
+                  onSubmit={this.handleSubmit}
                 >
-                    {({
-                        values,
-                        errors,
-                        touched,
-                        handleChange,
-                        handleBlur,
-                        handleSubmit,
-                        isSubmitting,
-						
-                    }) => (
-                            <form onSubmit={handleSubmit} >
-                                <h1>Login</h1>
-                                <Field
-                                    name="email"
-                                    label="Email*"
-                                    placeholder="Email"
-                                    component={EmailInput}
-                  />
-                            </form>
-                        )}
+                  {(formikProps: FormikProps<MessageFormValues>)=>{
+                    const {handleSubmit} = formikProps;
+ 
+                    return(
+                      <FormGroup onSubmit={handleSubmit}>
+                        <Field
+                            name="email"
+                            label="Email*"
+                            placeholder="Email"
+                            component={EmailInput}
+                        />
+                        <Field
+                            name="password"
+                            label="Password*"
+                            placeholder="Password"
+                            component={PasswordInput}
+                        />
+
+                    );
+                  }}
+ 
                 </Formik>
-            </DivForm>
+          </MessageFormContainer>
         );
     }
 }
-
-export default Loginform;
+ 
+export default LoginFormForm;
