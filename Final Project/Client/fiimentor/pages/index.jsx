937d8b58fcd3defff6475a1da6886d06957c69c1
AdelinaAlicia <62259221+AdelinaAlicia@users.noreply.github.com>
2020-04-08 14:18:05 +0300

    Newww

--- Final Project/Client/fiimentor/pages/index.jsx
+++ Final Project/Client/fiimentor/pages/index.jsx
@@ -1,13 +1,12 @@
 import Head from 'next/head'
 import React from 'react'
-import LoginForm from '../main/components/LoginFormComponents/LoginForm'
+
 const Home = () => (
   <>
   <Head>
   <title>FIIMentor</title>
 		</Head>
   <h1>FIIMentor</h1>
-  <LoginForm/>
 	</>
 );
 export default Home
