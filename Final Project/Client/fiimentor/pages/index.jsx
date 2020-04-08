2f4ff2f2917f68b08fc47d1fee7f9c0b6b42e5c1
VladAfrasinei <45604842+VladAfrasinei@users.noreply.github.com>
2020-04-08 15:35:33 +0300

    deleted import ForgotPasswordFrom

--- Final Project/Client/fiimentor/pages/index.jsx
+++ Final Project/Client/fiimentor/pages/index.jsx
@@ -1,15 +1,12 @@
 import Head from 'next/head'
 import React from 'react'
-import  ForgotPasswordForm  from '../main/components/ForgotPasswordComponent/ForgotPasswordForm'
 
 const Home = () => (
   <>
   <Head>
   <title>FIIMentor</title>
 		</Head>
-  <h1>FIIMentor</h1>
-  <ForgotPasswordForm/>
-    
+  <h1>FIIMentor</h1> 
 	</>
 );
 export default Home
