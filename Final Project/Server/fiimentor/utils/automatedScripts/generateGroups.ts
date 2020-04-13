4aa5189661e92bbc6e587d172b49145a7d19493f
stefanxd99 <stefandanila67@gmail.com>
2020-04-13 20:19:28 +0300

    Script All student groups

--- Final Project/Server/fiimentor/utils/automatedScripts/generateGroups.ts
+++ Final Project/Server/fiimentor/utils/automatedScripts/generateGroups.ts
@@ -1,11 +1,13 @@
 import { createConnection } from "typeorm";
 import { generateProfessorsGroup } from "./allProfessorsGroup";
 import { generateUsersGroup } from "./allUsersGroup";
+import {generateStudentsGroup} from "./allStudentGroups";
 
 createConnection().then(async () => {
 
     await generateUsersGroup();
     await generateProfessorsGroup();
+    await generateStudentsGroup();
 
 }).catch(error => {
     console.log(error);
