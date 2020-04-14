c3cbf8ef0ef8522169a69ef8e3068ff2ddc8add5
AdelinaAlicia <62259221+AdelinaAlicia@users.noreply.github.com>
2020-04-15 00:43:36 +0300

    Creating a post thunk

---
+++ Final Project/Client/fiimentor/main/store/Message/createAPostThunk.js
@@ -0,0 +1,32 @@
+import {Dispatch} from 'redux';
+import Swal from 'sweetalert2';
+import {
+    createPostAction,
+    createPostSuccessAction,
+    createPostErrorAction,
+} from './postActions';
+
+import {Context} from '../../Context';
+import type { RegisterUserRequest } from '../../core/services/ApiService';
+import type { UserLogged } from '../../../global';
+
+export  const registerUserThunk = (messageContent:)=> async(
+    dispatch: Dispatch
+)=>{
+    try{
+        dispatch(createPostAction());
+
+        const post: UserLogged = await Context.apiService.createPost(userCredentials);
+
+        dispatch(createPostSuccessAction(post));
+    } catch(e) {
+        dispatch(createPostErrorAction(e));
+
+        Swal.fire({
+            title: 'Error!',
+            text: 'There was an error on creating a post!',
+            type: 'error',
+            confirmButtonText: 'Ok',
+        })
+    }
+};
