a080e5126b6b492ece6b71ef8138f62d068026bb
poggonet01 <artiompogonet98@gmail.com>
2020-04-15 22:09:36 +0700

    Functionalitate / Lista profesori

--- Final Project/Server/fiimentor/controllers/index.ts
+++ Final Project/Server/fiimentor/controllers/index.ts
@@ -1,11 +1,6 @@
 import {getAllUsers} from './userController'
 import {login, register, registerRole} from './authController'
 import {createPost} from './postController'
-<<<<<<< HEAD
 import {getProfessorList} from './professorController'
-export {getAllUsers,getProfessorList,login, register, createPost,registerRole}
-=======
 import {getGroupeList} from "./groupeController";
-
-export {getAllUsers, login, register, createPost, registerRole, getGroupeList}
->>>>>>> Develop
+export {getAllUsers, login, register, createPost, registerRole, getGroupeList,getProfessorList}
