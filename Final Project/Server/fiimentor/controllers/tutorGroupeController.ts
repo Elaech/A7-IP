e87e5faa4173d04ed3ce1d7c956e489d897d96ef
PristavuDragos <62354493+PristavuDragos@users.noreply.github.com>
2020-04-15 21:45:20 +0300

    no message

--- Final Project/Server/fiimentor/controllers/tutorGroupeController.ts
+++ Final Project/Server/fiimentor/controllers/tutorGroupeController.ts
@@ -1,20 +1,26 @@
 import {GroupeRepository} from "../Repositories/GroupeRepository";
 import HttpStatus from "http-status-codes";
 
-async function tutorGroupeList(req:null,res:any) {
-    const groupeRepository=new GroupeRepository();
-    const list=await groupeRepository.getAll();
-    let index = list.length - 1;
-    while (index >= 0) {
-        if (list[index].ownerId === 0) {
-            list.splice(index, 1);
+async function tutorGroupeList(req:any,res:any) {
+    try {
+        const groupeRepository = new GroupeRepository();
+        const list = await groupeRepository.getAll();
+        let index = list.length - 1;
+        while (index >= 0) {
+            if (list[index].ownerId === 0) {
+                list.splice(index, 1);
+            }
+            index -= 1;
         }
-        index -= 1;
-    }
-    return res.status(HttpStatus.OK).json({
-        list:list
-    });
-
+        return res.status(HttpStatus.OK).json({
+            list: list
+        });
+    }catch(error) {
+        req.log.error(`Unable to get tutor groups -> ${req.url} -> ${error}`);
+        return res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({
+            success: false,
+        });
+    };
 }
 
 const utils={tutorGroupeList};
