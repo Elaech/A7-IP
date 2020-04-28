1dffd7f99b39b487b76caee9ebdcce5906933af8
paulscutaru <61314329+paulscutaru@users.noreply.github.com>
2020-04-28 16:23:22 +0300

    Changes
    Deleted !important and moved ViewPosts from index

--- Final Project/Client/fiimentor/main/components/ViewPostsComponent/ViewPostsStyle.jsx
+++ Final Project/Client/fiimentor/main/components/ViewPostsComponent/ViewPostsStyle.jsx
@@ -2,12 +2,12 @@ import styled from 'styled-components';
 import {CSSProperties} from 'react';
 
 export const TableContainer = styled.div`
-    font: 15px Spectral!important;
-    width: 50%!important;
+    font: 15px Spectral;
+    width: 50%;
     background:#F3F9FB;
-    margin-left: 1rem!important;
-    border:#D4D4CE 1px solid!important;
-    border-radius: 35px!important;
-    padding: 0px 25px 25px 25px!important;
-    box-shadow: 1px 2px 5px 2px #D4D4CE!important;
+    margin-left: 1rem;
+    border:#D4D4CE 1px solid;
+    border-radius: 35px;
+    padding: 0px 25px 25px 25px;
+    box-shadow: 1px 2px 5px 2px #D4D4CE;
 `;
