909e368135aa0e7074cc94c56fd65cfe0fd33b6e
denisabadarau <61699357+denisabadarau@users.noreply.github.com>
2020-04-28 13:42:42 +0300

    create create the details form after registration
    (without  modal)

---
+++ Final Project/Client/fiimentor/main/components/FormAfterRegistration/FormAfterRegistration.jsx
@@ -0,0 +1,113 @@
+import React from 'react';
+import {FastField, Field, Formik, FormikProps} from 'formik';
+import * as Yup from 'yup';
+import {Button} from '@material-ui/core';
+import {FormGroup} from '../LoginFormComponents/LoginFormStyles';
+import {FormContainer, buttonStyles} from './FormAfterRegistrationStyles';
+
+import {SelectOption} from '../Generics/Select/SelectOption';
+import {Select} from '../Generics/Select/Select';
+import {options1,options11,options12,options13} from './SelectOptionsForm';
+
+
+
+interface AfterRegistrationFormValues {
+    role1: SelectOption;
+}
+
+
+const initialValues: AfterRegistrationFormValues = {
+    role1: [SelectOption.create()],
+};
+
+
+const validationSchema: Yup.Schema<AfterRegistrationFormValues> = Yup.object().shape({
+    role1: Yup.string()
+        .required('Alege rolul!'),
+    
+});
+
+
+class AfterRegistrationForm extends React.Component<Props> {
+    handleSubmit = (values: AfterRegistrationFormValues)=> {
+        console.log(values);
+
+     }
+
+    render() {
+        return (
+            <FormContainer>
+                <Formik
+                    initialValues={initialValues}
+                    validationSchema={validationSchema}
+                    onSubmit={this.handleSubmit}
+                >
+                    {(formikProps: FormikProps<AfterRegistrationFormValues>) => {
+                         const {
+                            handleSubmit,
+                            values: {role1, role11, role12, role13}
+                        } = formikProps;
+
+                        return (
+                            <FormGroup onSubmit={handleSubmit}>
+
+                                <FastField
+                                    label="Rol:"
+                                    name="role1"
+                                    options={options1}
+                                    closeMenuOnSelect
+                                    component={Select}
+                                />
+
+                                {role1  && (role1.label === 'Student' || role1.label === 'Tutore') &&
+                                (<div>
+                                            <FastField
+                                                label="An:"
+                                                name="role11"
+                                                options={options11}
+                                                closeMenuOnSelect
+                                                component={Select}
+                                            />
+                                            <FastField
+                                                label="Semian:"
+                                                name="role12"
+                                                options={options12}
+                                                closeMenuOnSelect
+                                                component={Select}
+                                            />
+                                            <FastField
+                                                label="Grupa:"
+                                                name="role13"
+                                                options={options13}
+                                                closeMenuOnSelect
+                                                component={Select}
+                                            />
+                                        </div>)
+                                }
+
+                                <Button
+                                    type="submit"
+                                    style={buttonStyles}
+                                    variant="contained"
+                                >
+                                    Submit
+                                </Button>
+                                
+                                
+                            </FormGroup>
+                        );
+                    }}
+
+                </Formik>
+            </FormContainer>
+        );
+    }
+
+    
+}
+
+export default AfterRegistrationForm;
+
+
+
+
---
+++ Final Project/Client/fiimentor/main/components/FormAfterRegistration/SelectOptionsForm.js
@@ -0,0 +1,31 @@
+import {SelectOption} from '../Generics/Select/SelectOption';
+
+export const options1: SelectOption[] = [
+    { name: 'Student', label: 'Student', value: 'Student' },
+    { name: 'Profesor', label: 'Profesor', value: 'Profesor' },
+    { name: 'Tutore', label: 'Tutore', value: 'Tutore' },
+];
+
+
+export const options11: SelectOption[] = [
+    { name: '1', label: '1', value: '1', parentName: 'An' },
+    { name: '2', label: '2', value: '2', parentName: 'An' },
+    { name: '3', label: '3', value: '3', parentName: 'An' },
+];
+
+export const options12: SelectOption[] = [
+    { name: 'A', label: 'A', value: 'A', parentName: 'Litera' },
+    { name: 'B', label: 'B', value: 'B', parentName: 'Litera' },
+    { name: 'E', label: 'E', value: 'E', parentName: 'Litera' },
+];
+
+export const options13: SelectOption[] = [
+    { name: '1', label: '1', value: '1', parentName: 'Numar' },
+    { name: '2', label: '2', value: '2', parentName: 'Numar' },
+    { name: '3', label: '3', value: '3', parentName: 'Numar' },
+    { name: '4', label: '4', value: '4', parentName: 'Numar' },
+    { name: '5', label: '5', value: '5', parentName: 'Numar' },
+    { name: '6', label: '6', value: '6', parentName: 'Numar' },
+    { name: '7', label: '7', value: '7', parentName: 'Numar' },
+
+]
