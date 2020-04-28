2f48d6f66dc8e09107ab90b0e546aa5d0baa4208
GafencoMaria <61698634+GafencoMaria@users.noreply.github.com>
2020-04-28 18:27:45 +0300

    SideBar

--- Final Project/Client/fiimentor/pages/index.jsx
+++ Final Project/Client/fiimentor/pages/index.jsx
@@ -1,11 +1,12 @@
 import Head from 'next/head'
 import React from 'react'
-
+import LoginPageLayout from '../main/components/LoginPageLayout/LoginPageLayout'
 const Home = () => (
-    <>
+  <div>
     <Head>
       <title>FIIMentor</title>
     </Head>
-    </>
+    <LoginPageLayout/>
+    </div>
 );
 export default Home
