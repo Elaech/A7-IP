6d303ebb7b13014fad5288575009da60812867cc
Alexandra Blaga <61698515+blagaalexandra@users.noreply.github.com>
2020-04-28 14:52:28 +0300

    SearchAPIService

--- Final Project/Client/fiimentor/main/core/services/ApiService.js
+++ Final Project/Client/fiimentor/main/core/services/ApiService.js
@@ -51,6 +51,18 @@ export interface CreatePostRequest {
     groupe?: GroupePost;
 }
 
+export interface SearchRequest {
+    query: String;
+    page: ' ';
+    size: int;
+    filters: Filters;
+}
+
+export interface Filters {
+    professors?: ProfessorsPost;
+    groupe?: GroupePost;
+}
+
 export interface ApiService {
     registerUser(req: RegisterUserRequest): Promise<User>;
 
@@ -67,7 +79,6 @@ export interface ApiService {
     getProfesori(authorizer: string): Promise<Profesor[]>;
 
     getGrupeMentorat(autorizer: string): Promise<any>;
-
 
     createPost(req: CreatePostRequest): Promise<void>;
 
