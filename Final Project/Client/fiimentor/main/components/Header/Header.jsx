3c4d9a5c67e32aba0f72cb339173af72273258ac
paulscutaru <61314329+paulscutaru@users.noreply.github.com>
2020-05-09 20:36:29 +0300

    Log Out

--- Final Project/Client/fiimentor/main/components/Header/Header.jsx
+++ Final Project/Client/fiimentor/main/components/Header/Header.jsx
@@ -1,14 +1,27 @@
 import React from 'react';
 import {LogoHome} from '../../globalStyledVariables';
 import {HeaderStyles, LogoContainer} from './HeaderStyles';
+import {Button} from "@material-ui/core";
+import {Context} from '../../Context';
 
 export class Header extends React.Component<> {
+    logout = () => {
+        sessionStorage.removeItem("userToken");
+        sessionStorage.removeItem("user");
+        sessionStorage.clear();
+        
+        console.log("Log out successful.")
+        Context.routerService.replace('/');
+    };
+
     render() {
+        
         return (
             <HeaderStyles>
                 <LogoContainer>
                     <LogoHome/>
                 </LogoContainer>
+                <Button variant="contained" size="large" onClick={this.logout}>Log out</Button>
             </HeaderStyles>
         );
     }
